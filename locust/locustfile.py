d5720df509dea07c0dcc63b73c77b30642d4cb21
Fabian Iancu <iancufabian45@gmail.com>
2026-01-09 23:33:32 +0200

    write locustfile and dockerfile for it

---
+++ locust/locustfile.py
@@ -0,0 +1,19 @@
+import json
+import random
+from locust import HttpUser, task, between
+
+with open("data/texts.json", "r") as f:
+    SAMPLE_TEXTS = json.load(f)
+
+class SentimentUser(HttpUser):
+    wait_time = between(1, 3)
+
+    @task
+    def predict_sentiment(self):
+        payload = {"text": random.choice(SAMPLE_TEXTS)}
+
+        self.client.post(
+            "/predict",
+            json=payload,
+            headers={"Content-Type": "application/json"}
+        )
