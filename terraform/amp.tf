a4127d1284b96dd698c3890aec95716a5950b1c2
Fabian Iancu <iancufabian45@gmail.com>
2026-02-18 16:23:15 +0200

    enable agent mode for local prometheus

---
+++ terraform/amp.tf
@@ -0,0 +1,71 @@
+locals {
+  amp_remote_write_endpoint = "${aws_prometheus_workspace.this.prometheus_endpoint}api/v1/remote_write"
+}
+
+
+#namespace
+
+resource "kubernetes_namespace_v1" "monitoring" {
+  metadata {
+    name = var.prometheus_namespace
+  }
+  depends_on = [
+    module.eks
+  ]
+}
+
+resource "helm_release" "prometheus" {
+  name       = "kube-prometheus-stack"
+  namespace  = kubernetes_namespace_v1.monitoring.metadata[0].name
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "kube-prometheus-stack"
+  version    = var.prometheus_chart_version
+
+  values = [
+    yamlencode({
+      prometheus = {
+        serviceAccount = {
+          create = false
+          name   = var.prometheus_service_account_name
+        }
+
+        prometheusSpec = {
+          replicas = 1
+
+          retention = "1h"
+          enableFeatures = [
+            "agent"
+          ]
+
+
+          storageSpec = null
+
+          remoteWrite = [
+            {
+              url = "${aws_prometheus_workspace.this.prometheus_endpoint}api/v1/remote_write"
+              sigv4 = {
+                region = var.aws_region
+              }
+            }
+          ]
+        }
+      }
+
+      grafana = {
+        enabled = false
+      }
+
+      alertmanager = {
+        enabled = false
+      }
+    })
+  ]
+}
+
+resource "aws_prometheus_workspace" "this" {
+  alias = var.amp_workspace_alias
+
+  tags = {
+    Project = var.project_name
+  }
+}
